a505bf9f41dd12cb63144635c6f535cffd3c9e76
Darin Wallace <dwalleye66@gmail.com>
2021-01-03 17:39:47 -0600

    db change

--- db/schema.sql
+++ db/schema.sql
@@ -2,8 +2,33 @@ DROP DATABASE IF EXISTS easy_serve_db;
 CREATE DATABASE easy_serve_db;
 USE easy_serve_db;
 
-CREATE TABLE food (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(45) NOT NULL,  
+CREATE TABLE products (
+  hotdogName Andy Secular,
+  hotdogName The Burner,
+  hotdogName Philly Steak,
+  hotdogName Chihuahua,
+  hotdogName BLT Dog,
+  hotdogName Kitchen Sink,
+  hotdogName Bio Diesel,
+  hotdogName Corn Dog,
+  hotdogName Meat Candy,
+  drinkName Coke Cola,
+  drinkName Dr Pepper,
+  drinkName Sprite,
+  drinkName Diet Coke,
+  drinkName Diet Sprite,
+  drinkName Water,
+  sidesName French Fries,
+  sidesName Cajun Gumbo Fries,
+  sidesName Sweet Potato Fries,
+  sidesName Kimchi,
+  icecreamName Vanilla,
+  icecreamName Chocolate,
+  icecreamName Strawberry,
 );
-
+CREATE TABLE hotdog ingredients(
+  ingredientName bun,
+  ingredientName toppings,
+  ingredientName meat,
+  ingredientName sauce,
+);
