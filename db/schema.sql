c9bd6074e415a74e0f2569fbfce2a29eb37f26ce
Darin Wallace <dwalleye66@gmail.com>
2021-01-03 20:54:06 -0600

    db adaptation

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,6 @@
 DROP DATABASE IF EXISTS easy_serve_db;
 CREATE DATABASE easy_serve_db;
 
-
 CREATE TABLE products (
   hotdogName Andy Secular,
   hotdogName The Burner,
--- models/hotdogs.js
+++ models/hotdogs.js
@@ -1,4 +1,4 @@
-// Requiring bcrypt for password hashing. Using the bcryptjs version as the regular bcrypt module sometimes causes errors on Windows machines.
+// Requiring bcrypt for password hashing. Using the bcryptjs version as the regular bcrypt module sometimes causes errors on Windows machines
 const bcrypt = require('bcryptjs');
 // Creating our Hotdog Menu and Options
 module.exports = function (sequelize, DataTypes) {
