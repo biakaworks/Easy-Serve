70533ecf8018352dc4bf1a723271b70a9795294a
Donovan Chavez <chavezdon17@gmail.com>
2021-01-04 22:24:37 -0600

    made API routes

--- routes/api/connection.js
+++ routes/api/connection.js
@@ -1,19 +1,56 @@
-var mysql = require("mysql");
+const router = require("express").Router();
+var hotdogJS = require("../../models/hotdogs.js");
+var table = require("../../models/table.js");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: " easy_serve_db",
+// Add sequelize code to get all food and return them as JSON
+router.get("/api/tables", async (req, res) => {
+  const hotdog = await hotdogJS.findAll({});
+  res.json(hotdog);
+});
+router.post("/api/tables", async function (req, res) {
+  const newTable = await table.create(req.body);
+  res.json(newTable);
 });
 
-connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+router.delete("/api/hotDogs/:id", async (req, res) => {
+  const option = {
+    where: {
+      id: req.params.id,
+    },
+  };
+  const result = await hotdogJS.destroy(req.body);
+  res.json();
 });
 
-module.exports = connection;
+// Add sequelize code to get a specific food and return it as JSON
+router.get("/api/:food", async (req, res) => {
+  const selectItem = await table.findOne({
+    where: {
+      id: req.params.food,
+    },
+  });
+  res.json(selectItem);
+});
+
+// POST route for saving a food. We can create food with the data in req.body
+router.post("/api/hotDogs", async function (req, res) {
+  const newHotDog = await Hotdogs.create(req.body);
+  res.json(newHotDog);
+});
+
+router.delete("/api/food/:id", async (req, res) => {
+  const option = {
+    where: {
+      id: req.params.id,
+    },
+  };
+  const result = await Hotdogs.destroy(req.body);
+  res.json();
+});
+
+// Add sequelize code to create a order
+router.post("/api/newOrder", async (req, res) => {
+  const createOrder = await Food.create(req.body);
+  res.json(createOrder);
+});
+module.exports = router;
