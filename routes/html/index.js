7b3a0da8014319ebfde6c762e6a258a804d890ad
Biaka Z <biaka@biakaworks.com>
2021-01-07 21:36:47 -0600

    updated HTML folder location

--- routes/html/index.js
+++ routes/html/index.js
@@ -15,32 +15,32 @@ const router = require('express').Router();
 // In each of the below cases the user is shown an HTML page of content
 // ---------------------------------------------------------------------------
 
-router.get('/home', (_req, res) => {
-  res.sendFile(path.join(__dirname, '../public/home.html'));
+router.get('/', (_req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'home.html'));
 });
 
 router.get('/table1', (_req, res) => {
-  res.sendFile(path.join(__dirname, '../public/table1.html'));
+  res.sendFile(path.join(__dirname, 'views', 'table1.html'));
 });
 
 router.get('/table2', (_req, res) => {
-  res.sendFile(path.join(__dirname, '../public/table2.html'));
+  res.sendFile(path.join(__dirname, 'views', 'table2.html'));
 });
 
 router.get('/table3', (_req, res) => {
-  res.sendFile(path.join(__dirname, '../public/table3.html'));
+  res.sendFile(path.join(__dirname, 'views', 'table3.html'));
 });
 
 router.get('/table4', (_req, res) => {
-  res.sendFile(path.join(__dirname, '../public/table4.html'));
+  res.sendFile(path.join(__dirname, 'views', 'table4.html'));
 });
 
 router.get('/table5', (_req, res) => {
-  res.sendFile(path.join(__dirname, '../public/table5.html'));
+  res.sendFile(path.join(__dirname, 'views', 'table5.html'));
 });
 
 router.get('/table6', (_req, res) => {
-  res.sendFile(path.join(__dirname, '../public/table6.html'));
+  res.sendFile(path.join(__dirname, 'views', 'table6.html'));
 });
 
 module.exports = router;
