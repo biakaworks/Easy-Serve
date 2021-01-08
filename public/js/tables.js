7f3f445df47dd5060afd432fa47318a2287fd2ed
Myles Burkemper <myles.burkemper@gmail.com>
2021-01-07 18:13:06 -0600

    more css updates

---
+++ public/js/tables.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.dropdown-trigger').dropdown();
+  });
+  
+$(document).ready(function(){
+    $('.modal').modal();
+  });
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
